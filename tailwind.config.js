b67a305f8f61a22f5b1e33e322ffcb889e00776a
alifrayhan <alifrayhan92@gmail.com>
2024-11-13 21:59:44 +0600

    header add

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  darkMode: "media",
+  content: ["*"],
   theme: {
+    colors: {
+      'orange': '#f44335',
+      'white': '#ffffff',
+      'black': '#000000',
+      'navy': '#2A3663',
+      'dark': '#2d3142',
+      'smokey': '#f5f5f5',
+    },
     extend: {},
   },
   plugins: [],
